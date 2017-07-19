06d86f63d77a1d3ed4eed4028bcb6962062ba52b
Alou Toure <alou_toure@yahoo.com>
2017-07-18 20:37:40 -0500

    Added personal information form (userInfor.html)

--- src/main/java/com/toure/project/LakeBank/models/User.java
+++ src/main/java/com/toure/project/LakeBank/models/User.java
@@ -1,13 +1,17 @@
 package com.toure.project.LakeBank.models;
 
 import org.hibernate.validator.constraints.Email;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
  * Created by Alou on 7/12/2017.
  */
+
+@Entity
 public class User {
 
     @NotNull
@@ -19,11 +23,33 @@ public class User {
     @NotNull
     @Email(message = "Please enter a valid email address")
     private String email;
+    @NotNull
+    @Size(min = 9, max = 9)
+    private int ssn;
+    @NotNull
+    @Size(min = 5, max = 5)
+    private  int zip;
+    @NotNull
+    @Size(max = 30)
+    private String firstName;
+    @NotNull
+    @Size(max = 30)
+    private String lastName;
+    @NotNull
+    @Size(max = 50)
+    private String address;
+    @NotNull
+    @Size(max = 30)
+    private String city;
+    private String state;
+    @Size(max = 10)
+    private String phone;
 
     public User(String username, String password, String email){
         this.username = username;
         this.password = password;
         this.email = email;
+
     }
 
     public User(){
@@ -52,5 +78,55 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getSsn() { return ssn; }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
